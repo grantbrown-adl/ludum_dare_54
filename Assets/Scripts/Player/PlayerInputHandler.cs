6ce26a38731b0c4d6b6701422b0289cc20049c8e
Grant Brown <grant.brown@outlook.com>
2023-09-30 17:59:58 +0930

    adds player shooting implementation
     - Added ability to shoot
     - Added a couple of tags

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -6,20 +6,41 @@ using UnityEngine;
 public class PlayerInputHandler : MonoBehaviour
 {
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private bool _canFire;
+    [SerializeField] private float _fireRate;
     private void Awake()
     {
         _playerController = GetComponent<PlayerController>();
+        _canFire = true;
+        if (_fireRate <= 0) _fireRate = 0.5f;
     }
 
     void Update()
     {
-        RecieveInput();
+        ReceiveInput();
     }
 
-    void RecieveInput()
+    void ReceiveInput()
     {
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-        _playerController.HandleInput(new Vector2(x, y));
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 moveInput = new Vector2(x, y);
+
+        bool fire = _canFire && Input.GetButtonDown("Fire1");
+        
+        _playerController.HandleInput(moveInput, fire);
+
+        if(fire)
+        {
+            _canFire = false;
+            StartCoroutine(FireCooldown());
+        }
+    }
+
+
+    private IEnumerator FireCooldown()
+    {
+        yield return new WaitForSeconds(_fireRate);
+        _canFire = true;
     }
 }
